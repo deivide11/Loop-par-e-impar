f386e7f35e5831c16e210711f0289e2b219b381f
Deivide Hernique <deivideh03.com@gmail.com>
2024-09-19 02:40:12 -0300

    Loop com par e impar

---
+++ script.js
@@ -0,0 +1,11 @@
+//Escreva um loop for ou while que exiba
+// qual número é par e qual é número impar
+
+for(let i = 0; i <= 50; i++){
+
+    if(i % 2 == 0){
+        console.log(`O número ${i} é par`)
+    } else {
+        console.log(`O número ${i} é ímpar`)
+    }
+}
