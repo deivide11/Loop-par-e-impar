d475117f0c407d6f769f6395d1acafc939f0c892
DEIVIDE HENRIQUE <0201392321041>
2024-09-19 10:20:06 -0300

    Loop par e impar

--- script.js
+++ script.js
@@ -6,6 +6,6 @@ for(let i = 0; i <= 50; i++){
     if(i % 2 == 0){
         console.log(`O número ${i} é par`)
     } else {
-        console.log(`O número ${i} é ímpar`)
+        console.log(`O número ${i} é impar`)
     }
 }
